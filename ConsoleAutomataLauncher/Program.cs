34ed552a44265da856c45671248181c087eee173
kakrulit <sav1920@mail.ru>
2025-05-03 17:11:02 +0300

    try

--- ConsoleAutomataLauncher/Program.cs
+++ ConsoleAutomataLauncher/Program.cs
@@ -11,20 +11,57 @@ internal class Program
 {
     private static void Main()
     {
+        ModelView mv = new RmModelView();
+
+        ViewModel vm = new RmViewModel();
+        vm.BoundaryConditions = CellularAutomaton.BoundaryConditions.Bounce;
+        (vm as RmViewModel).Iterations = 32;
+        (vm as RmViewModel).Name = ViewModel.AutomataName.ReleaseModel;
+        (vm as RmViewModel).Size = (300, 300, 1);
+        (vm as RmViewModel).Diameter = 32f;
+        (vm as RmViewModel).SolidMass = 23f;
+        (vm as RmViewModel).LiquidMass = 12f;
+        (vm as RmViewModel).D = 0.001f;
+        (vm as RmViewModel).K = 0.0001f;
+
+        CellularAutomaton relModel = new RmModelTest(mv, vm);
+        relModel.InitializeAutomata();
+
+        Render2D render = new Render2D(mv.FieldAG, 3);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            if (File.Exists("output.txt"))
+            {
+                Thread.Sleep(3000);
+                break;
+            }
+
+            relModel.Update();
+            render.Draw(mv.FieldAG);
+        }
+
+        Raylib.CloseWindow();
+
+        DataToExcel();
+    }
+
+    private static void DataToExcel()
+    {
         string inputPath = "output.txt";
         string outputPath = "output.csv";
 
         if (!File.Exists(inputPath))
         {
-            Console.WriteLine("Файл output.txt не найден!");
-            return;
+            File.Create(inputPath).Dispose();
+            Console.WriteLine("Файл output.txt не найден. Создан пустой файл.");
         }
 
         string[] lines = File.ReadAllLines(inputPath);
 
-        using (var writer = new StreamWriter(outputPath))
+        using (var writer = new StreamWriter(outputPath, false, new System.Text.UTF8Encoding(true)))
         {
-            writer.WriteLine("Индекс;Значение"); // Заголовки
+            writer.WriteLine("№ итерации;Масса жидких");
 
             foreach (var line in lines)
             {
@@ -39,32 +76,5 @@ internal class Program
         }
 
         Console.WriteLine($"CSV-файл сохранён: {Path.GetFullPath(outputPath)}");
-
-
-        //ModelView mv = new RmModelView();
-
-        //ViewModel vm = new RmViewModel();
-        //vm.BoundaryConditions = CellularAutomaton.BoundaryConditions.Bounce;
-        //(vm as RmViewModel).Iterations = 1000;
-        //(vm as RmViewModel).Name = ViewModel.AutomataName.ReleaseModel;
-        //(vm as RmViewModel).Size = (300, 300, 1);
-        //(vm as RmViewModel).Diameter = 32f;
-        //(vm as RmViewModel).SolidMass = 23f;
-        //(vm as RmViewModel).LiquidMass = 12f;
-        //(vm as RmViewModel).D = 0.001f;
-        //(vm as RmViewModel).K = 0.0001f;
-
-        //CellularAutomaton relModel = new RmModelTest(mv, vm);
-        //relModel.InitializeAutomata();
-
-        //Render2D render = new Render2D(mv.FieldAG, 3);
-
-        //while (!Raylib.WindowShouldClose())
-        //{
-        //    relModel.Update();
-        //    render.Draw(mv.FieldAG);
-        //}
-
-        //Raylib.CloseWindow();
     }
 }
